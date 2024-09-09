42d924febde3ab4d291ce525f486feec605984ba
asserhanafy <asserhanafy68@gmail.com>
2024-09-10 01:42:12 +0300

    Modify topic names

--- ros2_ws/src/weather_pkg/weather_pkg/humidity_node.py
+++ ros2_ws/src/weather_pkg/weather_pkg/humidity_node.py
@@ -6,7 +6,7 @@ from example_interfaces.msg import String
 class HumidityNode(Node):
     def __init__(self):
         super().__init__("humidity_node")
-        self.publisher_ = self.create_publisher(String, "robot_news", 10)
+        self.publisher_ = self.create_publisher(String, "humidity", 10)
         self.timer_ = self.create_timer(0.5, self.publish_news)
         self.get_logger().info("Humidity sensor has been started")
 
--- ros2_ws/src/weather_pkg/weather_pkg/pressure_node.py
+++ ros2_ws/src/weather_pkg/weather_pkg/pressure_node.py
@@ -6,7 +6,7 @@ from example_interfaces.msg import String
 class PressureNode(Node):
     def __init__(self):
         super().__init__("pressure_node")
-        self.publisher_ = self.create_publisher(String, "robot_news", 10)
+        self.publisher_ = self.create_publisher(String, "pressure", 10)
         self.timer_ = self.create_timer(0.5, self.publish_news)
         self.get_logger().info("Pressure sensor has been started")
 
