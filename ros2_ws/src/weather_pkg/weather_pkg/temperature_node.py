3d22be3164a6833de0da7d0fc483c8508b04bced
asserhanafy <asserhanafy68@gmail.com>
2024-09-10 00:40:02 +0300

    Modify Temperature Node

---
+++ ros2_ws/src/weather_pkg/weather_pkg/temperature_node.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+class TemperatureNode(Node):
+    def __init__(self):
+        super().__init__("temperature_node")
+        self.min_temp_ = 10
+        self.max_temp_ = 100
+        self.publisher_ = self.create_publisher(String, "robot_news", 10)
+        self.timer_ = self.create_timer(0.5, self.publish_news)
+        self.get_logger().info("Temperature sensor has been started")
+
+    def publish_news(self):
+        msg = String()
+        msg.data = "25"
+        self.publisher_.publish(msg)
+        msg.data = "5"
+        self.publisher_.publish(msg)
+        msg.data = "50"
+        self.publisher_.publish(msg)
+        msg.data = "150"
+        self.publisher_.publish(msg)
+        msg.data = "75"
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = TemperatureNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
