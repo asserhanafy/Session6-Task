55cf12e3500f9f20f2cb772e017cfcb245b90947
asserhanafy <asserhanafy68@gmail.com>
2024-09-10 00:52:44 +0300

    Modify Pressure Node

---
+++ ros2_ws/src/weather_pkg/weather_pkg/pressure_node.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+from example_interfaces.msg import String
+
+class PressureNode(Node):
+    def __init__(self):
+        super().__init__("pressure_node")
+        self.publisher_ = self.create_publisher(String, "robot_news", 10)
+        self.timer_ = self.create_timer(0.5, self.publish_news)
+        self.get_logger().info("Pressure sensor has been started")
+
+    def publish_news(self):
+        msg = String()
+        msg.data = "1.0"
+        self.publisher_.publish(msg)
+        msg.data = "0.9"
+        self.publisher_.publish(msg)
+        msg.data = "1.1"
+        self.publisher_.publish(msg)
+        msg.data = "1.3"
+        self.publisher_.publish(msg)
+        msg.data = "1.15"
+        self.publisher_.publish(msg)
+
+def main(args=None):
+    rclpy.init(args=args)
+    node = PressureNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
